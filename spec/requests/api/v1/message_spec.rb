689c0bad5ec573f28de898863a262a98baab60db
Mehrdad Sami <MehrdadSaami@gmail.com>
2023-06-08 18:37:13 +0330

    Write test for payload

--- spec/requests/api/v1/message_spec.rb
+++ spec/requests/api/v1/message_spec.rb
@@ -17,5 +17,13 @@ RSpec.describe "Api::V1::Messages", type: :request do
       expect(response).to have_http_status(422)
       expect(json_body["errors"].key?("gateway")).to be true
     end
+
+    it "stores the payload" do
+      file = fixture_file_upload("spec/fixtures/image.jpeg")
+      post "/api/v1/message",
+           params: { message: { gateway: :slack, file:, payload: { message: "Hi" } } }
+      expect(response).to have_http_status(204)
+      expect(Message.last.payload).to eq({ "message" => "Hi" })
+    end
   end
 end
