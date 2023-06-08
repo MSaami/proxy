bc81aa62ff71b77e3ea22de875586b1e7f058a8b
Mehrdad Sami <MehrdadSaami@gmail.com>
2023-06-08 17:13:39 +0330

    Write some test for message model

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -21,3 +21,41 @@ RSpec.describe Message, type: :model do
     it { is_expected.to validate_content_type_of(:file)
       .allowing('text/csv', 'image/png', 'application/pdf', 'image/jpeg') }
   end
+
+  describe '#publish' do
+    it 'publishes message and get done' do
+      message = create(:message, gateway: :slack)
+      expect { message.publish! }.to change { message.status }.from('processing').to("done")
+    end
+
+    it 'publishes message and set result' do
+      message = create(:message, gateway: :slack)
+      expect { message.publish! }.to change { message.result }.from(nil)
+    end
+
+    it 'set status failed by exception if has error' do
+      message = create(:message, gateway: :slack)
+      expect(Gateway::GatewayFactory).to receive(:make_for).with(message)
+      expect { message.publish! }.to change {message.status}.from('processing').to('failed')
+    end
+  end
+
+  describe '.after_create' do
+    it 'pushes to queue after create a message' do
+      message = create(:message)
+      assert_enqueued_with(job: MessageSenderJob, args: [message.id])
+    end
+  end
+
+  describe 'change status' do
+    it 'updates status to done with result' do
+      message = create(:message)
+      expect {message.done_by_result('Done')}.to change {message.done?}.to(true)
+    end
+
+    it 'updates status to failed with result' do
+      message = create(:message)
+      expect {message.failed_by_exception('UUID')}.to change {message.failed?}.to(true)
+    end
+  end
+end
